0f7cad78ad2e04f139fa14ae4a2be7637bd3f940
Jacob Handy <43211690+jacobhandy3@users.noreply.github.com>
2021-01-02 17:59:33 -0600

    Front end sign up functionality
    Users can now sign up from the front end.

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -1,13 +1,57 @@
 import React from 'react';
-import axios from 'axios';
+import axiosInstance from "../axiosAPI";
 import { Modal,Button,Form } from "react-bootstrap";
 
+//interface for new users
+interface InewUser {
+    username: string,
+    password: string,
+    passwordConfirm: string,
+}
+const defaultUser:InewUser = {username:"",password:"",passwordConfirm:""}
 function SignUpModal() {
-
+    const [user,setUser]: [InewUser, (user:InewUser) => void] = React.useState(defaultUser);
     const [show, setShow]: [boolean, (show:boolean) => void] = React.useState<boolean>(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const handleUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({
+            username: e.target.value,
+            password: user.password,
+            passwordConfirm: user.passwordConfirm
+        })
+    }
+    const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({
+            username: user.username,
+            password: e.target.value,
+            passwordConfirm: user.passwordConfirm
+        })
+    }
+    const handlePasswordConfirm = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({
+            username: user.username,
+            password: user.password,
+            passwordConfirm: e.target.value,
+        })
+    }
+    const handleSubmit = async () => {
+        try{
+            if(user.password === user.passwordConfirm){
+                const response = await axiosInstance.post('accounts/profile/create/', {
+                    username: user.username,
+                    password: user.password,
+                });
+                console.log(response);
+                window.location.reload();
+            }
+            else{
+                console.log("Password does not match confirmation password!");
+            }
+        }
+        catch(error){throw error;}
+    }
 
     return (
         <>
@@ -22,13 +66,17 @@ function SignUpModal() {
                     <Form>
                         <Form.Group controlId="formBasicUsername">
                             <Form.Label>Username</Form.Label>
-                            <Form.Control placeholder="Enter username" />
+                            <Form.Control placeholder="Enter username" value={user.username} onChange={handleUsername}/>
                         </Form.Group>
                         <Form.Group controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control type="password" placeholder="Password" value={user.password} onChange={handlePassword}/>
                         </Form.Group>
-                        <Button variant="primary" type="submit">Submit</Button>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" value={user.passwordConfirm} onChange={handlePasswordConfirm}/>
+                        </Form.Group>
+                        <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
@@ -40,5 +88,3 @@ function SignUpModal() {
 }
 
 export default SignUpModal;
-// TODO:
-//      Sign Up Functionality
